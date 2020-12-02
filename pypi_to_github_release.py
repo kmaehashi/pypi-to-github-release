e28afd6d95b0ad30a16868960ff522e71a175c14
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-12-02 20:17:07 +0900

    support hash other than md5

--- pypi_to_github_release.py
+++ pypi_to_github_release.py
@@ -26,34 +26,32 @@ all_projects = [
 
 all_versions = [
     # PyPI / GitHub release
-    ('6.0.0rc1', 'v6.0.0rc1'),
-    ('6.0.0b3',  'v6.0.0b3'),
-    ('6.0.0b2',  'v6.0.0b2'),
-    ('6.0.0b1',  'v6.0.0b1'),
-    ('6.0.0a1',  'v6.0.0a1'),
-    ('5.0.0rc1', 'v5.0.0rc1'),
-    ('5.0.0b4',  'v5.0.0b4'),
-    ('5.0.0b3',  'v5.0.0b3'),
-    ('5.0.0b2',  'v5.0.0b2'),
-    ('5.0.0b1',  'v5.0.0b1'),
-    ('5.0.0a1',  'v5.0.0a1'),
-    ('4.0.0rc1', 'v4.0.0rc1'),
-    ('4.0.0b4',  'v4.0.0b4'),
+    ('9.0.0a2', 'v9.0.0a2'),
+#    ('6.0.0rc1', 'v6.0.0rc1'),
+#    ('6.0.0b3',  'v6.0.0b3'),
+#    ('6.0.0b2',  'v6.0.0b2'),
+#    ('6.0.0b1',  'v6.0.0b1'),
+#    ('6.0.0a1',  'v6.0.0a1'),
+#    ('5.0.0rc1', 'v5.0.0rc1'),
+#    ('5.0.0b4',  'v5.0.0b4'),
+#    ('5.0.0b3',  'v5.0.0b3'),
+#    ('5.0.0b2',  'v5.0.0b2'),
+#    ('5.0.0b1',  'v5.0.0b1'),
+#    ('5.0.0a1',  'v5.0.0a1'),
+#    ('4.0.0rc1', 'v4.0.0rc1'),
+#    ('4.0.0b4',  'v4.0.0b4'),
 ]
 
 def get_pypi_assets(project, version):
-    # Return a dict of URL and MD5 digest.
+    # Return a dict of URL to tuple ('hash-algorithm', 'hash').
     locator = distlib.locators.PyPIJSONLocator('https://pypi.org/pypi')
     proj = locator.get_project(project)
     if version not in proj:
         return {}
-
-    digests = proj[version].digests
-    assert all([x[0] == 'md5' for x in digests.values()])
-    return {url: x[1] for url, x in proj[version].digests.items()}
+    return proj[version].digests
 
 
-def upload_url_to_github_release(url, md5, release):
+def upload_url_to_github_release(url, algo, hash, release):
     filename = os.path.basename(url)
     for existing_asset in release.raw_data['assets']:
         if existing_asset['name'] == filename:
@@ -61,13 +59,13 @@ def upload_url_to_github_release(url, md5, release):
             return
 
     with tempfile.NamedTemporaryFile() as f:
-        h = hashlib.new('md5')
+        h = hashlib.new(algo)
         print('  >> downloading', url)
         with urllib.request.urlopen(url) as response:
             buf = response.read()
             f.write(buf)
             h.update(buf)
-        assert h.hexdigest() == md5
+        assert h.hexdigest() == hash
 
         print('  >> uploading')
         release.upload_asset(f.name, name=filename)
@@ -89,9 +87,9 @@ def main():
 
             gh_release = repo.get_release(version_gh)
             print(gh_release)
-            for url, md5 in sources.items():
-                print(f'Uploading: {url} (md5: {md5})')
-                upload_url_to_github_release(url, md5, gh_release)
+            for url, (algo, hash) in sources.items():
+                print(f'Uploading: {url} ({algo}: {hash})')
+                upload_url_to_github_release(url, algo, hash, gh_release)
 
 
 if __name__ == '__main__':
